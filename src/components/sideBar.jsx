4e7ad02db11859cef33e65dc7a96211cd594cc42
Sagudelo01 <andresagudelolon2003@gmail.com>
2026-01-31 02:48:34 -0500

    feat: enhance sidebar with active navigation and improved styling

--- src/components/sideBar.jsx
+++ src/components/sideBar.jsx
@@ -1,26 +1,26 @@
 // IMPORTACIONES
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import * as MdIcons from "react-icons/md";
 const SideBar = () => {
   return (
-    <div className="sidebar">
+    <div className="sidebar bg-light">
       <ul>
         <li>
-          <Link to="/"className="text-dark">
+          <NavLink to="/" className="text-dark rounded py-2 w-100 d-block px-3" activeClassName="active">
             <MdIcons.MdHouse className="me-2"/>Inicio
-          </Link>
+          </NavLink>
         </li>
 
         <li>
-          <Link to="/sales"className="text-dark">
+          <NavLink to="/sales" className="text-dark rounded py-2 w-100 d-block px-3" activeClassName="active">
             <MdIcons.MdBarChart className="me-2"/>Ventas
-          </Link>
+          </NavLink>
         </li>
 
         <li>
-          <Link to="/clients"className="text-dark">
+          <NavLink to="/clients" className="text-dark rounded py-2 w-100 d-block px-3" activeClassName="active">
             <MdIcons.MdPeopleAlt className="me-2"/>Clientes
-          </Link>
+          </NavLink>
         </li>
       </ul>
     </div>
